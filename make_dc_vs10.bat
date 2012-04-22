f3411869bf97a4003fe9c9e647028e8f91c64d6f
Andrew West <andrew@keyboardcowboy.co.uk>
2012-04-22 12:18:40 +0100

    - Updated to support Visual Studio 2010

--- make_dc_vs10.bat
+++ make_dc_vs10.bat
@@ -1,12 +1,11 @@
 @echo off
 @if "%fasm%"=="" goto :error_no_fasm
+@if "%VS100COMNTOOLS%"=="" goto :error_no_vstools
+@call "%VS100COMNTOOLS%\vsvars32.bat"
 
-@if "%VS90COMNTOOLS%"=="" goto :error_no_vstools
-@call "%VS90COMNTOOLS%\vsvars32.bat"
-
-devenv dcrypt.sln /Build "Release|win32"
+devenv dcrypt_vs10.sln /Build "Release|win32"
 @if errorlevel 1 goto :error_build_fail
-devenv dcrypt.sln /Build "Release|x64"
+devenv dcrypt_vs10.sln /Build "Release|x64"
 @if errorlevel 1 goto :error_build_fail
 
 
@@ -16,7 +15,7 @@ devenv dcrypt.sln /Build "Release|x64"
 @goto end
 :error_no_vstools
 @set errorlevel=1
-@echo ERROR: VS90COMNTOOLS variable is not set. 
+@echo ERROR: VS100COMNTOOLS variable is not set. 
 @goto end
 :error_build_fail
 @echo ERROR: Can not build solution. 
