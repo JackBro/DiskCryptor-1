b76ac42ff20a2dd758c2c838d56967e2f0e38e32
Andrew West <andrew@keyboardcowboy.co.uk>
2012-04-22 11:19:59 +0100

    - Converted project to Visual Studio 2010

--- make_dc.bat
+++ make_dc.bat
@@ -1,6 +1,15 @@
 @echo off
-@if "%VS90COMNTOOLS%"=="" goto :error_no_vstools
-@call "%VS90COMNTOOLS%\vsvars32.bat"
+@set vs_tools_found=0
+@if not "%VS100COMNTOOLS%"=="" (
+    @call "%VS100COMNTOOLS%\vsvars32.bat"
+    @set vs_tools_found=1
+) else (
+    @if not "%VS90COMNTOOLS%"=="" (
+        @call "%VS90COMNTOOLS%\vsvars32.bat"
+        @set vs_tools_found=1
+    )
+)
+@if %vs_tools_found%==0 goto :error_no_vstools
 
 devenv dcrypt.sln /Build "Release|win32"
 @if errorlevel 1 goto :error_build_fail
@@ -11,7 +20,7 @@ devenv dcrypt.sln /Build "Release|x64"
 @goto end
 :error_no_vstools
 @set errorlevel=1
-@echo ERROR: VS90COMNTOOLS variable is not set. 
+@echo ERROR: Neither the VS90COMNTOOLS or VS100COMNTOOLS variable is set. 
 @goto end
 :error_build_fail
 @echo ERROR: Can not build solution. 
